a24b3759084bcb2d5e486416e9f42467ee269e79
DG <derpguy125@gmail.com>
2022-08-19 20:13:06 -0400

    complete replacement of "game_english/data"
    and uhh, porting over other code from other crap

--- src/Profile.cpp
+++ src/Profile.cpp
@@ -317,7 +317,7 @@ BOOL InitializeGame(void)
 	ClearPermitStage();
 	StartMapping();
 	InitFlags();
-	if (!TransferStage(13, 200, 10, 8))
+	if (!TransferStage(0, 100, 7, 12))
 	{
 	#if !defined(JAPANESE) && defined(FIX_BUGS) // The Aeon Genesis translation didn't translate this
 		Backend_ShowMessageBox("Error", "Failed to load stage");
